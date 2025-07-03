a824a5fd3fa80ec5b1bd73631a8cfb58be438c00
tentinqu <bhaiyamaafkardo@gmail.com>
2025-07-03 16:41:29 +0530

    pls work

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "gitopsactions23"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
